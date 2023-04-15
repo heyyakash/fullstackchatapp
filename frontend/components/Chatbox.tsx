d8cc88e569b957e770872b3e7c08076463c20d76
heyyakash <akashsharma2002@gmail.com>
2023-04-15 23:29:39 +0530

    new fix

--- frontend/components/Chatbox.tsx
+++ frontend/components/Chatbox.tsx
@@ -6,8 +6,8 @@ import { uname } from './Body'
 const Chatbox = ({username,message,direction}:{username:string,message:string,direction:string}) => {
 
   return (
-
-    <div className={`flex float-${direction} ${direction==="right"?"":"flex-row-reverse"} gap-4 clear-both my-2 text-white`}>
+ 
+    <div className={`flex ${direction==="right"?"float-right":"float-left"} ${direction==="right"?"":"flex-row-reverse"} gap-4 clear-both my-2 text-white`}>
       <div className={`flex flex-col ${direction==="right"?"items-end":"items-start"} gap-2`}>
         <p className="text-xl font-semibold">{username}</p>
         <div className={`${direction==="right"?"bg-blue-500":"bg-white/20"} text-xl font-semibold rounded-b-xl ${direction==="right"?"rounded-l-xl":"rounded-r-xl"} px-4 py-2`}>{message}</div>
